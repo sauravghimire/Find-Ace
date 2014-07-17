2a2f1eeb0542d0db28de79c0229ed4f8e7ad370d
Susan Sanjhya <susan.sanjhya@gmail.com>
2014-07-17 17:03:25 +0545

    Login activity for facebook login

--- app/src/main/java/com/example/whoslucky/app/ui/LoginActivity.java
+++ app/src/main/java/com/example/whoslucky/app/ui/LoginActivity.java
@@ -10,9 +10,14 @@ import android.widget.Button;
 
 import com.example.whoslucky.app.MainActivity;
 import com.example.whoslucky.app.R;
+import com.example.whoslucky.app.utils.AppText;
 import com.sromku.simple.fb.Permission;
 import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.entities.Profile;
 import com.sromku.simple.fb.listeners.OnLoginListener;
+import com.sromku.simple.fb.listeners.OnProfileListener;
+import com.sromku.simple.fb.utils.Attributes;
+import com.sromku.simple.fb.utils.PictureAttributes;
 
 /**
  * Created by susan on 7/17/14.
@@ -29,9 +34,15 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login_acitivity);
 
-        progressDialog = new ProgressDialog(this);
         sharedPreferences = getSharedPreferences("FB_REG", 0);
+        String accessToken = sharedPreferences.getString(AppText.KEY_FB_ACCESS_TOKEN, "");
+        simpleFacebook = SimpleFacebook.getInstance(this);
+        if (simpleFacebook.isLogin() && simpleFacebook.getSession().getAccessToken().equals(accessToken)) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
 
+        progressDialog = new ProgressDialog(this);
         buttonFacebookLogin = (Button) findViewById(R.id.buttonFacebookLogin);
         buttonFacebookLogin.setOnClickListener(this);
 
@@ -54,7 +65,7 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
 
             case R.id.buttonSkipLogin:
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putBoolean("is_fb_login", false);
+                editor.putBoolean(AppText.KEY_IS_FB_LOGIN, false);
                 editor.commit();
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 finish();
@@ -65,7 +76,21 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
     private OnLoginListener loginListener = new OnLoginListener() {
         @Override
         public void onLogin() {
-            progressDialog.dismiss();
+            PictureAttributes pictureAttributes = Attributes.createPictureAttributes();
+            pictureAttributes.setWidth(150);
+            pictureAttributes.setHeight(150);
+            pictureAttributes.setType(PictureAttributes.PictureType.SQUARE);
+
+            Profile.Properties properties = new Profile.Properties.Builder()
+                    .add(Profile.Properties.NAME)
+                    .add(Profile.Properties.EMAIL)
+                    .add(Profile.Properties.BIRTHDAY)
+                    .add(Profile.Properties.PICTURE, pictureAttributes)
+                    .build();
+            progressDialog.setMessage("Getting Infos...");
+
+            simpleFacebook.getProfile(properties, profileListener);
+
         }
 
         @Override
@@ -88,4 +113,42 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
             progressDialog.dismiss();
         }
     };
+
+    private OnProfileListener profileListener = new OnProfileListener() {
+        @Override
+        public void onComplete(Profile response) {
+            super.onComplete(response);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(AppText.KEY_FB_ACCESS_TOKEN, simpleFacebook.getSession().getAccessToken());
+            editor.putString(AppText.KEY_FB_USER_NAME, response.getUsername());
+            editor.putBoolean(AppText.KEY_IS_FB_LOGIN, true);
+            editor.commit();
+            progressDialog.dismiss();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
+        @Override
+        public void onException(Throwable throwable) {
+            super.onException(throwable);
+            progressDialog.dismiss();
+        }
+
+        @Override
+        public void onFail(String reason) {
+            super.onFail(reason);
+            progressDialog.dismiss();
+        }
+
+        @Override
+        public void onThinking() {
+            super.onThinking();
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simpleFacebook = SimpleFacebook.getInstance(this);
+    }
 }
