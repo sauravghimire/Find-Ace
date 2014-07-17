fd209f50c4ca6189f0aac78bf47a18ee3330e1e9
Susan Sanjhya <susan.sanjhya@gmail.com>
2014-07-17 17:03:48 +0545

    new class to hold app text

---
+++ app/src/main/java/com/example/whoslucky/app/utils/AppText.java
@@ -0,0 +1,11 @@
+package com.example.whoslucky.app.utils;
+
+/**
+ * Created by susan on 7/17/14.
+ */
+public class AppText {
+    public static final String KEY_FB_PREF = "fb_pref";
+    public static final String KEY_IS_FB_LOGIN = "is_fb_login";
+    public static final String KEY_FB_ACCESS_TOKEN = "fb_access_token";
+    public static final String KEY_FB_USER_NAME = "fb_user_name";
+}
