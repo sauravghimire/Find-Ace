395b52653eda94a960a4c17a0f08936a0afe9ea4
Susan Sanjhya <susan.sanjhya@gmail.com>
2014-07-17 16:12:57 +0545

    facebook login added

---
+++ app/src/main/java/com/example/whoslucky/app/ui/SharedObjects.java
@@ -0,0 +1,11 @@
+package com.example.whoslucky.app.ui;
+
+import android.content.Context;
+
+/**
+ * Created by susan on 7/17/14.
+ */
+public class SharedObjects {
+
+    public static Context context;
+}
